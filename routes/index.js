863e172a2717e50ef22fd449badd0d927645d2b7
Maurizio Mangione <granzebru@gmail.com>
2013-05-16 13:49:49 +0200

    added async + 8a.nu feed parsing solved

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,23 @@
 exports.index = function(req, res){
 
-  // var parser = require('blindparser');
+  var parser = require('rssparser'),
+      async = require('async'),
+      options = {headers:{'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
 
-  // parser.parseURL('http://ws.audioscrobbler.com/1.0/user/granzebru/recenttracks.rss', function(err, out){
-  //   console.log(out);
-  //   console.log('=> errors: ' + err);
-  //   res.render('index', {lastfm: out});
-  // });
-
-
-  //http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df
-
-  var feed = require("feed-read");
-    feed("http://ws.audioscrobbler.com/1.0/user/granzebru/recenttracks.rss", function(err, articles) {
-      console.log(err);
-      console.log(articles);
-      res.render('index', {lastfm: articles});
+  async.parallel({
+    ottoa: function(callback){
+      parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out){
+        callback(null, out.items);
+      });
+    },
+    lastfm: function(callback){
+      parser.parseURL('http://ws.audioscrobbler.com/1.0/user/granzebru/recenttracks.rss', options, function(err, out){
+        callback(null, out.items);
+      });
+    }
+  },
+  function(err, results) {
+    res.render('index', results);
   });
 
 };
