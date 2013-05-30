187cb096a3164e1996abd1f4a77e00c29d16a7f8
Maurizio Mangione <granzebru@gmail.com>
2013-05-31 00:29:25 +0200

    feed output limited to 10 items

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ exports.index = function(req, res){
   async.parallel({
     ottoa: function(callback){
       parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out){
-        var ascentData = [];
-        for (var i = 0; i < out.items.length; i++) {
+        var ascentData = [],
+            items = out.items.slice(0,10);
+
+        for (var i = 0; i < items.length; i++) {
           var ascent = out.items[i].summary.split('<br>'),
               route = ascent[0].split(','),
               grade = route[0].slice(-3).trim(),
@@ -27,7 +29,7 @@ exports.index = function(req, res){
     },
     github: function(callback){
       parser.parseURL('https://github.com/Granze.atom', options, function(err, out){
-        callback(null, out.items);
+        callback(null, out.items.slice(0,10));
       });
     }
   },
