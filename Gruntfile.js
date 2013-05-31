f178029d1c994660496e3ab3a6ba534e3307ce12
Maurizio Mangione <granzebru@gmail.com>
2013-05-31 10:53:14 +0200

    grunt (nodemon, compass, watch, livereload) itegration

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodemon: {
+      dev: {}
+    },
+    compass: { //compile scss to css
+      dist: {
+        options: {
+          config: 'config.rb',
+          force: true
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['compass'],
+        options: {
+          livereload: 3000
+        }
+      }
+    },
+    concurrent: {
+      target: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('default', ['concurrent:target']);
+
+};
---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = '/'
+css_dir = 'public/css'
+sass_dir = 'public/sass'
+images_dir = 'public/img'
+javascripts_dir = 'routes'
+output_style = :compressed
+relative_assets= true
+line_comments = false
