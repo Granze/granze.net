77e5e34ffcd903fda2a86ba37cf07fbfc8714cb5
Maurizio Mangione <granzebru@gmail.com>
2013-11-30 22:10:08 +0100

    clean up + autoprefixer :v:

--- config.rb
+++ config.rb
@@ -4,8 +4,15 @@ sass_dir = 'public/sass'
 images_dir = 'public/img'
 fonts_dir = 'public/fonts'
 javascripts_dir = 'routes'
-output_style = :compressed
 relative_assets= true
 line_comments = false
 
-require 'compass-normalize'
+require 'autoprefixer-rails'
+require 'csso'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << Csso.optimize( AutoprefixerRails.compile(css) )
+  end
+end
