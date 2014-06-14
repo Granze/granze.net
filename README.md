b3d888c2a194eb51d228384d23f0bb59f3eaaf09
Maurizio Mangione <granzebru@gmail.com>
2014-06-14 19:08:53 +0200

    moved from grunt to gulp

--- README.md
+++ README.md
@@ -2,6 +2,3 @@ granze.net
 =========
 
 My personal website in nodejs + express
-
-
-P.S. I'm a Nodejs n00b so, if you have any suggestions, just drop me a line!
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    imagemin = require('gulp-imagemin'),
+    nodemon = require('gulp-nodemon'),
+    csso = require('gulp-csso'),
+    paths = {
+      css: ['public/components/Simple-Grid/simplegrid.css'],
+      scss: ['public/sass/*.scss'],
+      images: 'public/img/**/*'
+    };
+
+gulp.task('styles', function() {
+  return gulp.src(paths.css.concat(paths.scss))
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(concat('main.css'))
+      .pipe(autoprefixer())
+      .pipe(csso())
+      .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+      .pipe(plumber())
+      .pipe(imagemin())
+      .pipe(gulp.dest('public/img'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.images, ['images']);
+});
+
+gulp.task('node', ['styles', 'images'], function () {
+  nodemon({
+    script: 'app.js',
+    ext: 'js',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  }).on('start', ['watch'])
+      .on('change', ['watch'])
+      .on('restart', function () {
+        console.log('restarted!');
+      });
+});
+
+gulp.task('default', ['node']);
