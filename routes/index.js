6c7d3667fdbf0579da7df695a7b2d1d929cf3040
Maurizio Mangione <granzebru@gmail.com>
2014-06-17 23:01:19 +0200

    twitter bug fixed?

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-exports.index = function(req, res){
+exports.index = function(req, res) {
 
   'use strict';
 
@@ -7,66 +7,71 @@ exports.index = function(req, res){
       parser = require('rssparser2'),
       async = require('async'),
       relativeDate = require('relative-date'),
-      options = {headers:{'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
+      options = {headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
 
   twitter = new twitter(config.consumerKey, config.consumerSecret, config.accessToken, config.accessSecret, 3600);
 
   async.parallel({
-    ottoa: function(callback){
-      parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out){
-        if(err){
-           callback(null, err);
-        } else {
-          var ascentData = [],
-              items = out.items.slice(0,10);
+        ottoa: function(callback) {
+          parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out) {
+            if(err) {
+              callback(null, err);
+            } else {
+              var ascentData = [],
+                  items = out.items.slice(0, 10);
 
-          for (var i = 0; i < items.length; i++) {
-            var ascent = items[i].summary.split('<br>'),
-                route = ascent[0].split(','),
-                grade = route[0].slice(-3).trim(),
-                name = route[0].slice(0, -3).trim(),
-                crag = route[1],
-                date = items[i].time_ago;
+              for(var i = 0; i < items.length; i++) {
+                var ascent = items[i].summary.split('<br>'),
+                    route = ascent[0].split(','),
+                    grade = route[0].slice(-3).trim(),
+                    name = route[0].slice(0, -3).trim(),
+                    crag = route[1],
+                    date = items[i].time_ago;
 
-            ascentData.push({route: name, grade: grade, crag: crag, date: date});
-          }
-          callback(null, ascentData);
-        }
-      });
-    },
-    lastfm: function (callback) {
-      parser.parseURL('http://ws.audioscrobbler.com/1.0/user/granzebru/recenttracks.rss', function (err, out) {
-        if(err) {
-          callback(null, err);
-        } else {
-          callback(null, out.items);
-        }
-      });
-    },
-    github: function(callback){
-      parser.parseURL('https://github.com/Granze.atom', function(err, out){
-        if(err) {
-          callback(null, err);
-        } else {
-          callback(null, out.items.slice(0,10));
-        }
-      });
-    },
-    twitter: function(callback){
-      twitter.get('statuses/user_timeline', function(error, tweets) {
-        var tweetsData = [];
-        for (var i = 0; i < 10; i++) {
-          var created_at = tweets[i].created_at,
-              text = tweets[i].text,
+                ascentData.push({route: name, grade: grade, crag: crag, date: date});
+              }
+              callback(null, ascentData);
+            }
+          });
+        },
+        lastfm: function(callback) {
+          parser.parseURL('http://ws.audioscrobbler.com/1.0/user/granzebru/recenttracks.rss', function(err, out) {
+            if(err) {
+              callback(null, err);
+            } else {
+              callback(null, out.items);
+            }
+          });
+        },
+        github: function(callback) {
+          parser.parseURL('https://github.com/Granze.atom', function(err, out) {
+            if(err) {
+              callback(null, err);
+            } else {
+              callback(null, out.items.slice(0, 10));
+            }
+          });
+        },
+        twitter: function(callback) {
+          twitter.get('statuses/user_timeline', function(err, tweets) {
+            var tweetsData = [],
+                created_at,
+                text,
+                id_str,
+                i;
+
+            for(i = 0; i < 10; i++) {
+              created_at = tweets[i].created_at;
+              text = tweets[i].text;
               id_str = tweets[i].id_str;
 
-          tweetsData.push({date: relativeDate(new Date(created_at)), text: text, id: id_str});
+              tweetsData.push({date: relativeDate(new Date(created_at)), text: text, id: id_str});
+              if(i === 9) {callback(null, tweetsData);}
+            }
+          });
         }
-        callback(null, tweetsData);
+      },
+      function(err, results) {
+        res.render('index', results);
       });
-    }
-  },
-  function(err, results) {
-    res.render('index', results);
-  });
 };
