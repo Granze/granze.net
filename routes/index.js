db1e9d6611678f457f496b2cd5f4547c2fcf4c73
Maurizio Mangione <granzebru@gmail.com>
2016-01-31 23:51:32 +0100

    [WIP] rxjs

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,64 @@
 exports.index = function(req, res) {
-
   'use strict';
 
-  var Twit = require('twit'),
-      config = require('../config.js'),
-      parser = require('rssparser2'),
-      async = require('async'),
-      relativeDate = require('relative-date'),
-      options = {headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
+  var Twit = require('twit');
+  var config = require('../config.js');
+  var parser = require('rssparser2');
+  var Rx = require('rx');
+  var relativeDate = require('relative-date');
 
+  var options = {headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
+  var ottoaURL = 'http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df';
   var T = new Twit({consumer_key: config.consumerKey, consumer_secret: config.consumerSecret, access_token: config.accessToken, access_token_secret: config.accessSecret});
+  var parseURL = Rx.Observable.fromNodeCallback(parser.parseURL);
 
-  async.parallel({
-        ottoa: function(callback) {
-          parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out) {
-            if(err) {
-              callback(null, err);
-            } else {
-              var items = out.items.slice(0, 10);
+  //var ottoa$ = parseURL(ottoaURL, options, function(err, res) {
+  //  if(err) {
+  //    console.log(err);
+  //  } else {
+  //    var items = res.items.slice(0, 10);
+  //
+  //    var ascentData = items.map(function(item) {
+  //      var ascent = item.summary.split('<br>');
+  //      var route = ascent[0].split(',');
+  //
+  //      return {
+  //        route: route,
+  //        grade: route[0].slice(-3).trim(),
+  //        crag: route[1],
+  //        date: item.time_ago
+  //      };
+  //    });
+  //    return ascentData;
+  //  }
+  //});
 
-              var ascentData = items.map(function(item) {
-                var ascent = item.summary.split('<br>'),
-                    route = ascent[0].split(',');
+  var medium$ = parseURL('https://medium.com/feed/@granze')
+    .take(10)
+    .flatMap(function(rss) { return rss.items });
 
-                return {
-                  route: route,
-                  grade: route[0].slice(-3).trim(),
-                  crag: route[1],
-                  date: item.time_ago
-                };
-              });
-              callback(null, ascentData);
-            }
-          });
-        },
-        medium: function(callback) {
-          parser.parseURL('https://medium.com/feed/@granze', function(err, out) {
-            if(err) {
-              callback(null, err);
-            } else {
-              callback(null, out.items.slice(0, 10));
-            }
-          });
-        },
-        github: function(callback) {
-          parser.parseURL('https://github.com/Granze.atom', function(err, out) {
-            if(err) {
-              callback(null, err);
-            } else {
-              callback(null, out.items.slice(0, 10));
-            }
-          });
-        },
-        twitter: function(callback) {
-          T.get('statuses/user_timeline', {count: 10}, function(err, data) {
+  var github$ = parseURL('https://github.com/Granze.atom')
+    .take(1)
+    .flatMap(function(rss) { return rss.items });
 
-            var tweetsData = data.map(function(tweet) {
-              return {date: relativeDate(new Date(tweet.created_at)), text: tweet.text, id: tweet.id_str};
-            });
+  //var twitter = function() {
+  //  T.get('statuses/user_timeline', {count: 10}, function(err, res) {
+  //
+  //    var tweetsData = res.map(function(tweet) {
+  //      return {date: relativeDate(new Date(tweet.created_at)), text: tweet.text, id: tweet.id_str};
+  //    });
+  //
+  //    callback(null, tweetsData);
+  //  });
+  //};
 
-            callback(null, tweetsData);
-          });
-        }
-      },
-      function(err, results) {
-        res.render('index', results);
-      });
+  Rx.Observable.zip(
+    medium$,
+    github$
+  ).subscribe(
+    function(results){
+      return res.render('index', results);
+    }
+  );
+
 };
