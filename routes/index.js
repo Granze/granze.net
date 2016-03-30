fa4952c0cd0c4e44009a70b5807cabb01d7f7630
Maurizio Mangione <granzebru@gmail.com>
2016-03-30 15:30:13 +0200

    remove 8a.nu feed

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,11 @@ exports.index = function(req, res) {
       config = require('../config.js'),
       parser = require('rssparser2'),
       async = require('async'),
-      relativeDate = require('relative-date'),
-      options = {headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}};
+      relativeDate = require('relative-date');
 
   var T = new Twit({consumer_key: config.consumerKey, consumer_secret: config.consumerSecret, access_token: config.accessToken, access_token_secret: config.accessSecret});
 
   async.parallel({
-        ottoa: function(callback) {
-          parser.parseURL('http://www.8a.nu/rss/Main.aspx?UserId=19212&AscentType=0&ObjectClass=2&GID=3974d72911c05719152f0953e88cc2df', options, function(err, out) {
-            if(err) {
-              callback(null, err);
-            } else {
-              var items = out.items.slice(0, 10);
-
-              var ascentData = items.map(function(item) {
-                var ascent = item.summary.split('<br>'),
-                    route = ascent[0].split(',');
-
-                return {
-                  route: route,
-                  grade: route[0].slice(-3).trim(),
-                  crag: route[1],
-                  date: item.time_ago
-                };
-              });
-              callback(null, ascentData);
-            }
-          });
-        },
         medium: function(callback) {
           parser.parseURL('https://medium.com/feed/@granze', function(err, out) {
             if(err) {
