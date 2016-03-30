46fa48cda27187300156b3cdd58a1e38408d3bc7
Maurizio Mangione <granzebru@gmail.com>
2016-03-30 15:30:04 +0200

    remove deploy hook

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express'),
     routes = require('./routes'),
     hbs = require('hbs'),
     path = require('path'),
-    hookshot = require('hookshot'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
     bodyParser = require('body-parser'),
@@ -19,7 +18,6 @@ app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.use('/deploy', hookshot('refs/heads/master', 'cd ~/www/granze.net && git pull && touch tmp/restart.txt'));
 app.get('/', routes.index);
 app.get('/about', function(req, res){
   res.render('about');
